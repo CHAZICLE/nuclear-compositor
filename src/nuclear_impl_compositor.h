659d9dd83e9fa44f35be6197dead111e0f49aaff
Charles Howard <chazicle@gmail.com>
2017-06-04 12:30:30 +0100

    Fix wl_compositor implementation

--- src/nuclear_impl_compositor.h
+++ src/nuclear_impl_compositor.h
@@ -4,16 +4,6 @@
 #include <wayland-server.h>
 
 void nuclear_impl_compositor_bind(struct wl_client *client, void *data, uint32_t version, uint32_t id);
-static struct wl_compositor_interface nuclear_impl_compositor_interface;
-static void nuclear_impl_compositor_create_surface(struct wl_client *client,
-		       struct wl_resource *resource,
-		       uint32_t id);
-static void nuclear_impl_compositor_create_region(struct wl_client *client,
-		      struct wl_resource *resource,
-		      uint32_t id);
-static void nuclear_impl_compositor_delete_surface(struct wl_resource *resource);
-
-static struct wl_surface_interface nuclear_impl_surface_interface;
-static struct wl_region_interface nuclear_impl_region_interface;
+void nuclear_impl_region_bind(struct wl_client *client, void *data, uint32_t version, uint32_t id);
 
 #endif
